8aa825edfc3d5dcc064dd92ba9282115effedbda
Prince <prince@Princes-MacBook-Pro.local>
2018-08-08 13:54:30 -0400

    log in

--- src/Component/Forms/SignUp.js
+++ src/Component/Forms/SignUp.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+
 require('./cssForms.css')
+
 
 export default class SignUp extends Component {
   render() {
     return (
-        <div className='container'>
-        <div className='forms'>
-        <Form>
-        <Form.Field>
-                <label>First Name</label>
-                <input placeholder='firstName ' />
-        </Form.Field>
-        <Form.Field>
-                <label>Last Name</label>
-                <input placeholder='lastName ' />
-        </Form.Field>
-            <Form.Field>
-                <label>Email</label>
-                <input placeholder='Email ' />
-            </Form.Field>
-            <Form.Field>
-                <label>Password</label>
-                <input placeholder='Password' />
-            </Form.Field>
-            {true ? <Button type='submit'>Submit</Button> : <Button basic loading type='submit'>Submit</Button>}
-        </Form>
+            <div className='login-form'>
+            <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h2' color='blue' textAlign='center'>
+                Sign up
+                </Header>
+                <Form size='large'>
+                <Segment stacked>
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='First Name' />
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='Last Name' />
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                    <Form.Input
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password'
+                    />
+        
+                    <Button color='blue' fluid size='large'>
+                    Login
+                    </Button>
+                </Segment>
+                </Form>
+                <Message>
+                New to us? <a href='#'>Sign Up</a>
+                </Message>
+            </Grid.Column>
+            </Grid>
         </div>
-        </div>
-    )
-  }
-}
+        )
+    }
+    }
