6bd5f2cf0685ad122a262a72fbd7d1dde898c756
Ray Lee <raylee971227@gmail.com>
2018-08-08 16:38:14 -0400

    googleAuth added

---
+++ src/Firebase/googleAuth.js
@@ -0,0 +1,43 @@
+import * as firebase from 'firebase'
+import {auth} from './firebase'
+
+var provider = new firebase.auth.GoogleAuthProvider()
+provider.addScope('https://www.googleapis.com/auth/calendar')
+
+
+export const googleLogIn = async () => {
+  try {
+    await request = firebase.auth().signInWithPopup(provider);
+    // await auth.signInWithPopup(provider)
+    var token = request.credential.accessToken;
+    var user = request.user;
+  } catch (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var credential = err.credential;
+  }
+}
+
+// firebase.auth().signInWithPopup(provider).then(result => {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//   var user = result.user;
+//   // ...
+// }).catch(error => {
+//    // Handle Errors here.
+//    var errorCode = error.code;
+//    var errorMessage = error.message;
+//    // The email of the user's account used.
+//    var email = error.email;
+//    // The firebase.auth.AuthCredential type that was used.
+//    var credential = error.credential;
+//    // ...
+// })
+
+
+// firebase.auth().signOut().then(function() {
+//   // Sign-out successful.
+// }).catch(function(error) {
+//   // An error happened.
+// });
