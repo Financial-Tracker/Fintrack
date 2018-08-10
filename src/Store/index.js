4b898fe9c6ea0d5eda4740c6feabcd50a1d82c3f
vli8 <vli8@fordham.edu>
2018-08-10 10:06:01 -0400

    bug fixes

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,7 +1,9 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import createLogger from 'redux-logger'
+import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import plaidContainer from './plaidContainer'
+import userContainer from './userContainer'
 
 const reducer = combineReducers({
   plaidContainer,
