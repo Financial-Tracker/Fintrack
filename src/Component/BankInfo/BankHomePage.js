01969f5260322ae68bfdd95da66c71c5a23a7d3c
vli8 <vli8@fordham.edu>
2018-08-13 12:55:11 -0400

    Income bug fix

--- src/Component/BankInfo/BankHomePage.js
+++ src/Component/BankInfo/BankHomePage.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getPlaid, getDataFromFireStore } from '../../Store/plaidContainer'
-import Income from './Income'
 
-let counter = 0
 class BankHomePage extends React.Component {
     constructor(props) {
         super(props);
@@ -16,7 +14,6 @@ class BankHomePage extends React.Component {
         this.props.getFireStore()
     }
     render() {
-        counter++
         return (
             <div>
                 {Object.keys(this.props.plaidObj).length!==0? <div><h1>hi</h1></div> : <div></div>}
--- src/Component/BankInfo/Income.js
+++ src/Component/BankInfo/Income.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { List, Segment } from 'semantic-ui-react'
 import {connect} from 'react-redux'
+import {getDataFromFireStore } from '../../Store/plaidContainer'
+
 class Income extends React.Component{
 
   componentDidMount(){
@@ -9,35 +11,38 @@ class Income extends React.Component{
 
   render(){
     return (
-      <Segment inverted>
-        <List divided inverted relaxed>
-          <List.Item>
-            <List.Content>
-              <List.Header>Last year income</List.Header>
-              {}
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>Last year income before tax</List.Header>
-              {props.income.last_year_income_before_tax}
-            </List.Content>
-          </List.Item>
-  
-          <List.Item>
-            <List.Content>
-              <List.Header>Projected yearly income</List.Header>
-              {props.income.projected_yearly_income}
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>Projected yearly income before tax</List.Header>
-              {props.income.projected_yearly_income_before_tax}
-            </List.Content>
-          </List.Item>
-        </List>
-      </Segment>
+      <div>
+        {Object.keys(this.props.plaidObj).length!==0? 
+          <Segment inverted>
+          <List divided inverted relaxed>
+            <List.Item>
+              <List.Content>
+                <List.Header>Last year income</List.Header>
+                {this.props.plaidObj.income.last_year_income}
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content>
+                <List.Header>Last year income before tax</List.Header>
+                {this.props.plaidObj.income.last_year_income_before_tax}
+              </List.Content>
+            </List.Item>
+    
+            <List.Item>
+              <List.Content>
+                <List.Header>Projected yearly income</List.Header>
+                {this.props.plaidObj.income.projected_yearly_income}
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Content>
+                <List.Header>Projected yearly income before tax</List.Header>
+                {this.props.plaidObj.income.projected_yearly_income_before_tax}
+              </List.Content>
+            </List.Item>
+          </List>
+        </Segment>: <h1>Loading.... </h1> }
+      </div>
     )
   }
 }
