21a8339a0a05837475dc0211c0b99f45d425725a
vli8 <vli8@fordham.edu>
2018-08-13 15:52:36 -0400

    clean up of console logs

--- src/Store/plaidContainer.js
+++ src/Store/plaidContainer.js
@@ -10,19 +10,13 @@ export const getPlaid = (data) => {
 }
 export const getDataFromFireStore = () => async dispatch => {
     try {
-        console.log('firebase.auth(): ', firebase.auth())
-        console.log('firebase.auth().currentUser', firebase.auth().currentUser)
         firebase.auth().onAuthStateChanged( async(user) =>{
             if (user) {
                 // User is signed in.
                 const userEmail = firebase.auth().currentUser.email
-            console.log('user email: ', userEmail)
             const userRef = await firestore.collection('user').where('email', "==", userEmail.toString()).get()
-            console.log("user Ref:", userRef)
             const docRefId = await userRef.docs[0].id;
-            console.log("doc ref Id: ", docRefId)
             const dataAPI = await firestore.collection('user').doc("" + docRefId + "").get().then(user => user.data())
-            console.log(dataAPI, "AFTER BABY")
             const action = getPlaid(dataAPI)
             dispatch(action)
             } else {
