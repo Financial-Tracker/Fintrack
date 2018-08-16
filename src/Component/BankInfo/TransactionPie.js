47abd283e3fcedf8abbe9b28788e7714719d788a
vli8 <vli8@fordham.edu>
2018-08-16 12:35:28 -0400

    category chart added

--- src/Component/BankInfo/TransactionPie.js
+++ src/Component/BankInfo/TransactionPie.js
@@ -16,28 +16,97 @@ const brandColors = {
     lightGrey: '#e4e8ec'
 };
 class TransactionPie extends React.PureComponent{
-    categoryChart(){
-        let catArr = []
-        let hash = {}
-        for (let i=0; i<this.props.plaidData.transaction.length;i++){
-            if(!hash[this.props.plaidData.transaction[i].category[0]]){
-                hash[this.props.plaidData.transaction[i].category[0]] = this.props.plaidData.transaction[i].amount
+    constructor(props) {
+        super(props);
+    
+        this.mouseOverHandler = this.mouseOverHandler.bind(this);
+        this.mouseOutHandler = this.mouseOutHandler.bind(this);
+        this.mouseMoveHandler = this.mouseMoveHandler.bind(this);
+        this.toggleState = this.toggleState.bind(this);
+    
+        this.state = {
+            showToolTip: false,
+            componentWidth: 300
+            };
+            this.styles = {
+            '.pie-chart-lines': {
+                stroke: 'rgba(0, 0, 0, 1)',
+                strokeWidth: 1
+            },
+            '.pie-chart-text': {
+                fontSize: '10px',
+                fill: 'white'
             }
-            else{
-                hash[this.props.plaidData.transaction[i].category[0]] += this.props.plaidData.transaction[i].amount
+            };
+        }
+        
+        mouseOverHandler(d, e) {
+            this.setState({
+            showToolTip: true,
+            top: `${e.y - 10}px`,
+            left: `${e.x + 10}px`,
+            value: d.value,
+            key: d.data.key });
+        }
+        
+        mouseMoveHandler(e) {
+            if (this.state.showToolTip) {
+            this.setState({ top: `${e.y}px`, left: `${e.x + 10}px` });
             }
         }
-        console.log('HASH: ', hash)
-    }
+        
+        mouseOutHandler() {
+            this.setState({ showToolTip: false });
+        }
+        toggleState() {
+            this.setState({
+            active: !this.state.active
+            });
+        }
+        
+        createTooltipForCategories() {
+            if (this.state.showToolTip) {
+            return (
+                <ToolTip
+                top={this.state.top}
+                left={this.state.left}
+                >
+                You spend ${this.state.value} on {this.state.key}
+                </ToolTip>
+            );
+            }
+            return false;
+        }
+        categoryChart(){
+            let catArr = []
+            let hash = {}
+            for (let i=0; i<this.props.plaidData.transaction.length;i++){
+                if(!hash[this.props.plaidData.transaction[i].category[0]]){
+                    hash[this.props.plaidData.transaction[i].category[0]] = this.props.plaidData.transaction[i].amount
+                }
+                else{
+                    hash[this.props.plaidData.transaction[i].category[0]] += this.props.plaidData.transaction[i].amount
+                }
+            }
+            for(let key in hash){
+                catArr.push({key: key, value: hash[key]})
+            }
+            return catArr
+        }
+    
     render(){
         console.log('GOT IT! ', this.props.plaidData)
-        this.categoryChart()
         return(
-            <PieChart
-            labels
-            size={200} 
-            innerHoleSize = {100}
-            data={[{ key: 'A', value: 100 },{ key: 'B', value: 200 },{ key: 'C', value: 50 }]}/>
+            <div>
+                {this.createTooltipForCategories()}
+                <PieChart
+                size={200} 
+                innerHoleSize = {100}
+                data={this.categoryChart()}
+                mouseOverHandler={this.mouseOverHandler}
+                mouseOutHandler={this.mouseOutHandler}
+                mouseMoveHandler={this.mouseMoveHandler}/>
+            </div>
         )
     }
 }
