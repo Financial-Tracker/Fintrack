f2f2b93f4844fd3c5543be31601fb1c062d8404d
hamadou diallo <hmaadou_diallo@yahoo.com>
2018-08-20 11:21:10 -0400

    deleting goals

--- src/Store/GoalReducer.js
+++ src/Store/GoalReducer.js
@@ -3,17 +3,33 @@ import firebase from 'firebase'
 const db = firebase.firestore()
 const ADD_A_GOAL = 'ADD_A_GOAL'
 const GET_ALL_GOALS = 'GET_ALL_GOALS'
-
+const REMOVE_GOAL = 'REMOVE_GOAL'
 //Action Creaters
 const addingAGoalToReducer = (payload) => ({ type: ADD_A_GOAL, payload })
 const gotAllGoal = (payload) => ({ type: GET_ALL_GOALS, payload })
+const removeGoal = payload => ({ type: REMOVE_GOAL, payload })
 
 //Thunks
 export const addAGoal = (goal) => async dispatch => {
     try {
-        console.log(goal)
-        // go to fire base
-        dispatch(addingAGoalToReducer(goal))
+        firebase.auth().onAuthStateChanged(async user => {
+            if (user) {
+                const userEmail = user.email
+                const userRef = await db
+                    .collection("user")
+                    .where("email", "==", userEmail.toString())
+                    .get();
+                const docRefId = await userRef.docs[0].id;
+                const addingGoals = await db.collection('user').doc(docRefId.toString()).update({
+                    Goals: firebase.firestore.FieldValue.arrayUnion(goal)
+                })
+
+                dispatch(addingAGoalToReducer(goal))
+
+            }
+        })
+
+
     } catch (error) {
         console.log(error)
     }
@@ -36,6 +52,31 @@ export const getAllGoal = () => async dispatch => {
         console.log(error)
     }
 }
+export const destroyingGoal = goalId => async dispatch => {
+    // console.log(goalId)
+    // dispatch(removeGoal(goalId))
+    try {
+        firebase.auth().onAuthStateChanged(async user => {
+            if (user) {
+                const userEmail = user.email
+                const userRef = await db
+                    .collection("user")
+                    .where("email", "==", userEmail.toString())
+                    .get();
+                const docRefId = await userRef.docs[0].id;
+                const destroyGoals = await db.collection('user').doc(docRefId.toString()).update({
+                    Goals: firebase.firestore.FieldValue.arrayRemove(goalId)
+                })
+                // console.log('DESTRA', destroyGoals)
+                dispatch(removeGoal(goalId))
+
+            }
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 const initialState = {
     allGoals: [],
@@ -55,6 +96,13 @@ const GoalReducer = (state = initialState, action) => {
             return {
                 ...state,
                 allGoals: action.payload
+            }
+        case REMOVE_GOAL:
+            console.log('ACTION', action)
+            // return state
+            return {
+                ...state,
+                allGoals: state.allGoals.filter((goal, index) => index !== action.payload)
             }
         default:
             return state;
