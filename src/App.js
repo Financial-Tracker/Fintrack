c1765ce80681529b00e4c5d0aa5c4ff3dc094b84
Fig Yogurt <itisaftab@gmail.com>
2018-08-20 12:05:02 -0400

    fix little errors

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import store from "./Store";
 // import Income from "./Component/BankInfo/Income";
 // import TransactionTable from "./Component/BankInfo/TransactionTable";
 // import BankPage from "./Component/Pages/BankPage";
-// New Components imports 
+// New Components imports
 // import Nav from "./allComponents/smallComponents/Navbar";
 // import AddAccount from './allComponents/AddAccount'
 // import Balance from './allComponents/Balance'
@@ -30,65 +30,56 @@ import store from "./Store";
 // import Budgets from "./Component/BankInfo/Budgets";
 
 //----------------------------------------------------------------------------------------------------------
-//new imports 
-import BudgetPage from './allComponents/BudgetPage'
-import OverviewPage from './allComponents/OverviewPage'
-import TransactionPage from './allComponents/TransactionPage'
-import GoalPage from './allComponents/GoalPage'
-import WaysToSavePage from './allComponents/WaysToSavePage'
-import UserSettingPage from './allComponents/UserSettingPage'
-import EditGoal from './allComponents/EditGoal'
-import BillForm from './allComponents/smallComponents/BillForm'
-import GoalForm from './/allComponents/smallComponents/GoalForm'
+//new imports
+import BudgetPage from "./allComponents/BudgetPage";
+import OverviewPage from "./allComponents/OverviewPage";
+import TransactionPage from "./allComponents/TransactionPage";
+import GoalPage from "./allComponents/GoalPage";
+import WaysToSavePage from "./allComponents/WaysToSavePage";
+import UserSettingPage from "./allComponents/UserSettingPage";
+import EditGoal from "./allComponents/EditGoal";
+import BillForm from "./allComponents/smallComponents/BillForm";
+import GoalForm from ".//allComponents/smallComponents/GoalForm";
 //----------------------------------------------------------------------------------------------------------
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div>
-      <HashRouter >
-
-      {
-        !this.props._user ? 
-        (  <Switch>
-          {/* {!this.props._user ? (null) : (null) } */}
-          {/* Login and sign up  */}
-        <Route exact path='/login' component={LogIn} />
-        <Route exact path='/signup' component={SignUp} />
-        <Route component={LogIn} />
-      </Switch>)
-        : 
-        ( <Switch >
-          {/* once the user is logged in */}
-          <Route exact path='/' component ={OverviewPage} />
-          <Route exact path="/plaid" component={Plaid} />
-          <Route exact path='/transactions' component={TransactionPage} />
-          <Route exact path='/budget' component={BudgetPage} />
-          <Route exact path='/goal'component={GoalPage} />
-          <Route exact path='/waystosave' component={WaysToSavePage} />
-          <Route exact path='/settings' component={UserSettingPage} />
-          <Route exact path={`/editgoal/:Id`} component={EditGoal} />
-          <Route component={OverviewPage}/>
-        </Switch>)
-      }
-
-
-
-
-      </HashRouter>
-        <BillForm />
-        <GoalForm />
-      </div>
+        <div>
+          <HashRouter>
+            {!this.props._user ? (
+              <Switch>
+                {/* {!this.props._user ? (null) : (null) } */}
+                {/* Login and sign up  */}
+                <Route exact path="/login" component={LogIn} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route component={LogIn} />
+              </Switch>
+            ) : (
+              <Switch>
+                {/* once the user is logged in */}
+                <Route exact path="/" component={OverviewPage} />
+                <Route exact path="/plaid" component={Plaid} />
+                <Route exact path="/transactions" component={TransactionPage} />
+                <Route exact path="/budget" component={BudgetPage} />
+                <Route exact path="/goal" component={GoalPage} />
+                <Route exact path="/waystosave" component={WaysToSavePage} />
+                <Route exact path="/settings" component={UserSettingPage} />
+                <Route exact path={`/editgoal/:Id`} component={EditGoal} />
+                <Route component={OverviewPage} />
+              </Switch>
+            )}
+          </HashRouter>
+          <BillForm />
+          <GoalForm />
+        </div>
       </Provider>
-
     );
   }
 }
 
 export default withAuth(App);
-
-
 
 //       <Provider store={store}>
 //       <HashRouter>
@@ -100,7 +91,7 @@ export default withAuth(App);
 
 //         <div className='contant'>
 //         {
-//   this.props._user ? 
+//   this.props._user ?
 //   <Switch>
 //     <Route exact path="/!" component={Overview} />
 //     <Route exact path="/" component={Overview} />
@@ -112,14 +103,14 @@ export default withAuth(App);
 //     <Route exact path='/account' component={AddAccount} />
 //     <Route exact path='/budgets' component={Budgets} />
 //     <Route exact path='/goals' component={Goals} />
-//     <Route exact path='/profile' component={Profile} />   
-//     <Route exact path='/settings' component={Settings} />   
-//     <Route exact path='/transaction' component={Transactions} />  
+//     <Route exact path='/profile' component={Profile} />
+//     <Route exact path='/settings' component={Settings} />
+//     <Route exact path='/transaction' component={Transactions} />
 //     <Route exact path='/saves' component={WaysToSave} />
 //     <Route exact path='/bills' component={Bills} />
 //   </Switch>
-  
-//   : 
+
+//   :
 //   <Switch>
 //   <Route exact path="/!" component={LogIn} />
 //   <Route exact path="/login" component={LogIn} />
